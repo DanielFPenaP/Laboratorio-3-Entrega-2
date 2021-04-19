9bc6f34d4a86afabc5a0bc6a48fe856d5111752a
Daniel <daniel_fp1998@hotmail.com>
2021-04-18 19:42:03 -0500

    arreglo e implementacion comunicacion UDP

--- cliente.py
+++ cliente.py
@@ -4,21 +4,22 @@ import socket
 from datetime import datetime
 from time import time
 import os
-host = "192.168.1.62"
-port = 10000
-def connectToServer():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-    server_address = (host, port)
-    print('Connecting to {} port {}'.format(*server_address))
-    sock.connect(server_address)
-    return sock
 
 
-def helloProtocol(socket, log):
-    clientId, address = socket.recvfrom(4096)
-    socket.sendto(bytes("Ya recibi mi numero", "utf-8"),address)
-    clientId = clientId.decode("utf-8")
-    fileName = "Cliente"+ str(clientId) +"-.mp4"
+def sendDataToServer(data, socket: socket):
+    server_address = ('192.168.107.128', 10000)
+    socket.sendto(data, server_address)
+
+
+def helloProtocol(socket: socket, log):
+    print("enviando hola")
+    sendDataToServer(bytes("Hola", "utf-8"), socket)
+    print("recibiendo hola")
+    data, address = socket.recvfrom(40)
+    clientId = data.decode("utf-8")
+    
+    # socket.sendto(bytes("Ya recibi mi numero"+str(clientId), "utf-8"),address)
+    fileName = "Cliente" + str(clientId) + "-.mp4"
     log.write("El nombre de archivo recibido es: "+fileName+"\n")
     log.write("Mi numero de cliente es: " + clientId + "\n")
     return [fileName, clientId]
@@ -27,18 +28,15 @@ def helloProtocol(socket, log):
 def getHashFromFile(fileName):
     hash = hashlib.md5()
     file = open(fileName, "rb")
-    content = file.read(4096)
+    content = file.read(2048)
     while content:
-        content = file.read(4096)
+        content = file.read(2048)
         hash.update(content)
     return hash.digest()
 
 
-def checkHash(fileName):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    server.bind((host, port)) 
-    conecction, addr = server.accept()
-    input_data = socket.recv(4096)
+def checkHash(socket: socket, fileName):
+    input_data = socket.recvfrom(4096)
     if input_data:
         if isinstance(input_data, bytes):
             end = input_data == bytes("Ya termine", "utf-8")
@@ -48,12 +46,13 @@ def checkHash(fileName):
             hash = input_data
             calculateHash = getHashFromFile(fileName)
             comprobacion = hash == calculateHash
-            print("Hash recibido: "+ str(hash))
-            print("Hash calculado: "+ str(calculateHash))
-            print("Son iguales = " + str(comprobacion)) 
+            print("hash recibido: " + str(hash))
+            print("hash calculado: " + str(calculateHash))
+            print("Son iguales = " + str(comprobacion))
+            return comprobacion
 
 
-def saveFileFromServer(fileName, socket , clientId, log):
+def saveFileFromServer(fileName, socket, clientId, log):
     file = open(fileName, "wb")
     print("Esperando...")
     start_time = time()
@@ -61,10 +60,11 @@ def saveFileFromServer(fileName, socket , clientId, log):
     goodEnd = False
     while True:
         paquetes += 1
-        bytes_read, address = socket.recvfrom(4096)
+        bytes_read = socket.recv(4096)
         if not bytes_read:
             break
-        end = bytes_read[len(bytes_read) - 10:len(bytes_read)] == bytes("Ya termine", "utf-8")
+        end = bytes_read[len(bytes_read) - 10:len(bytes_read)
+                         ] == bytes("Ya termine", "utf-8")
         if not end:
             file.write(bytes_read)
         else:
@@ -72,34 +72,50 @@ def saveFileFromServer(fileName, socket , clientId, log):
             goodEnd = True
             break
 
-    if(goodEnd):     
+    if(goodEnd):
         elapsed_time = time() - start_time
-        log.write("El tiempo de transferencia del archivo "+fileName+" al cliente "+
-                clientId + " es: " + str(elapsed_time) + " segundos\n")    
-        log.write("Numero de paquetes enviados al cliente "+ clientId +" = " + str(paquetes) + " paquetes\n") 
+        log.write("El tiempo de transferencia del archivo "+fileName+" al cliente " +
+                  clientId + " es: " + str(elapsed_time) + " segundos\n")
+        log.write("Numero de paquetes enviados al cliente " +
+                  clientId + " = " + str(paquetes) + " paquetes\n")
         file.close()
-        print("El archivo se ha recibido correctamente.")
-        socket.sendto(bytes("Ya recibi", "utf-8"),address)
-        log.write("Transmision exitosa del cliente "+ clientId +"\n")
+        if checkHash(socket, fileName):
+            print("El archivo se ha recibido correctamente.")
+            sendDataToServer(bytes("Ya recibi", "utf-8"), socket)
+            log.write("Transmision exitosa del cliente " + clientId + "\n")
+        else:
+            print("El archivo NO se ha recibido correctamente.")
+            sendDataToServer(bytes("No recibi", "utf-8"), socket)
+            log.write("Transmision No exitosa del cliente " + clientId + "\n")
     else:
-        print("Error en la transmision")
-        log.write("Error en transmision del cliente "+ clientId +"\n")
+        print("Error en la transmicion")
+        sendDataToServer(bytes("No recibi", "utf-8"), socket)
+        log.write("Error en transmision del cliente " + clientId + "\n")
 
 
-def threadedC(id):
-    print('Inicio cliente')
-    socket = connectToServer()
+def startLogger(clientId):
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d-%H-%M-%S")
-    print("date and time =", dt_string)	
-    log = open(dt_string+"_client_"+str(id)+"-log.txt","a")
-    log.write("Comenzando Cliente\n")
-    fileName, clientId = helloProtocol(socket, log)
-    saveFileFromServer(fileName, socket, clientId, log)
+    print("date and time =", dt_string)
+    return open(dt_string+"_client_"+str(clientId)+"-log.txt", "a")
+
+
+def threadedCliente(id):
+    print('inicio cliente')
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    logger = startLogger(id)
+    fileName, clientId = helloProtocol(cliente, logger)
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d-%H-%M-%S")
+    print("date and time =", dt_string)
+    logger.write("Comenzando Cliente\n")
+    fileName, clientId = helloProtocol(cliente, logger)
+    saveFileFromServer(fileName, cliente, clientId, logger)
     sizefile = os.stat(fileName).st_size
-    log.write("Valor total de bytes enviados al cliente "+  clientId +" = "+str(sizefile)+" bytes\n")
-    log.close()
-    checkHash(socket, fileName)
+    logger.write("Valor total de bytes enviados al cliente " +
+                 clientId + " = "+str(sizefile)+" bytes\n")
+    logger.close()
+
     print('Cerrando socket')
     socket.close()
 
@@ -110,13 +126,14 @@ def Main():
     # print (clientNumber)
     thread_list = []
     for j in range(clientNumber):
-        thread = threading.Thread(target = threadedC, args = (j+1,))
+        thread = threading.Thread(target=threadedCliente, args=(j+1,))
         thread.start()
         thread_list.append(thread)
 
     for thread in thread_list:
         thread.join()
-    print("Se termino de recibir la informacion de todos los clientes")
+    print("Se termino de recivir la informacion de todos los clientes")
 
-if __name__ == '__main__': 
-    Main() 
+
+if __name__ == '__main__':
+    Main()
--- server.py
+++ server.py
@@ -1,66 +1,53 @@
+from io import TextIOWrapper
+import socket
 import threading
 import hashlib
-import socket
 from datetime import datetime
-from time import sleep, time
+from time import time
 import os
 
-# print_lock = threading.Lock()
-server = ""
-host = "192.168.1.62" 
-port = 10000
-def threaded(self,address, fileName, hash, clientId, log): 
-    
+
+def sendInfoThread(address, fileName, hash, clientId, logger: TextIOWrapper, server: socket):
     print("Enviando archivo "+fileName+" al cliente "+str(clientId))
     start_time = time()
     paquetes = 0
-    while True:   
-        file = open(fileName, "rb")
+    file = open(fileName, "rb")
+    content = file.read(4096)
+    while content:
+        server.sendto(content, address)
+        paquetes += 1
         content = file.read(4096)
-        while content:
-            self.server.sendto(content,address)
-            paquetes += 1
-            content = file.read(4096)      
-        break
-    print(paquetes)
-    self.server.sendto(bytes("Ya termine", "utf-8"),address)
-    print("Voy a recibir la confirmacion")
-    data, cliente  = self.server.recvfrom(4096)
-    elapsed_time = time() - start_time
-    if data == bytes("Ya recibi", "utf-8"):
-        print("Confimacion exitosa para cliente " + str(clientId))
-        log.write("Tranferencia Exitosa hacia el cliente "+ str(clientId)+"\n")
-    else:
-        print("Confimacion exitosa para cliente " + str(clientId))
-        log.write("Tranferencia No Exitosa hacia el cliente "+ str(clientId)+"\n")
-    log.write("El tiempo de transferencia del archivo "+fileName+" al cliente"+
-              str(clientId)+" es: "+str(elapsed_time)+" segundos\n")
-    log.write("Numero de paquetes enviados al cliente "+ str(clientId) +" = "+str(paquetes)+" paquetes\n")
-    sizefile = os.stat(fileName).st_size
-    log.write("Valor total de bytes enviados al cliente "+ str(clientId) +" = "+str(sizefile)+" bytes\n")
-    print("El cliente "+str(clientId) +" ha mandado: "+ str(cliente))
-    # conexion tcp para hash
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    server.bind((host, port)) 
-    conecction, addr = server.accept()
-    conecction.send(hash)
-    # Cerrar conexión y archivo.
-    conecction.close()
+    server.sendto(bytes("Ya termine", "utf-8"), address)
+    elapsed_time = time() - start_time
+    logger.write("El tiempo de transferencia del archivo "+fileName+" al cliente" +
+                 str(clientId)+" es: "+str(elapsed_time)+" segundos\n")
+    logger.write("Numero de paquetes enviados al cliente " +
+                 str(clientId) + " = "+str(paquetes)+" paquetes\n")
+    sizefile = os.stat(fileName).st_size
+    logger.write("Valor total de bytes enviados al cliente " +
+                 str(clientId) + " = "+str(sizefile)+" bytes\n")
     file.close()
-    print("El archivo ha sido enviado correctamente al usuario "+str(clientId))
-    # print_lock.release()
+    server.sendto(hash, address)
+
+
+def startLogger():
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d-%H-%M-%S")
+    print("date and time =", dt_string)
+    return open(str(dt_string)+"-Server-log.txt", "a")
+
 
 def getHashFromFile(fileName):
     hash = hashlib.md5()
     file = open(fileName, "rb")
-    content = file.read(4096)
+    content = file.read(2048)
     while content:
-        content = file.read(4096)
+        content = file.read(2048)
         hash.update(content)
     return hash.digest()
 
-    
+
 def getFileName(value):
     if value == "1":
         fileName = "infra/100 MB.mp4"
@@ -71,59 +58,86 @@ def getFileName(value):
     return fileName
 
 
-def Main(): 
-    now = datetime.now()
-    dt_string = now.strftime("%Y-%m-%d-%H-%M-%S")
-    print("date and time =", dt_string)	
-    log = open(str(dt_string)+"-Server-log.txt","a")
-    log.write("Comenzando servidor\n")
-    
-    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-    server.bind((host, port)) 
-    print("socket binded to port", port) 
-   
-    
-   # Menu de opciones
+def startServer(logger: TextIOWrapper):
+    logger.write("Comenzando servidor\n")
+    host = "192.168.107.128"
+    port = 10000
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server.bind((host, port))
+    print("socket binded to port", port)
+    return server
+
+
+def printMenu(logger: TextIOWrapper):
     print('Seleccione el archivo a utilizar')
     print('1. Archivo de 100MB')
     print('2. Archivo de 250MB')
     opcionA = input()
-    log.write("Nombre del archivo Enviado: ")
-    consoleMsg = ('Usted selecciono la opcion ' + opcionA) if opcionA == '1' or opcionA == '2' else 'Opcion de prueba'
+    logger.write("Nombre del archivo Enviado: ")
+    consoleMsg = ('Ustede selecciono la opcion ' +
+                  opcionA) if opcionA == '1' or opcionA == '2' else 'Opcion de prueba'
     print(consoleMsg)
-    fielName = getFileName(opcionA)
-    log.write(fielName+"\n")
+
+
+def getClients(logger: TextIOWrapper, server: socket, clientsNumber: int):
+    lista = []
+    clientId = 0
+    print("recibiendo clientes")
+    while clientsNumber > clientId:
+        data, addr = server.recvfrom(1024)
+        if data == bytes("Hola", "utf-8"):
+            clientId += 1
+            print('Connected to :', addr[0], ':', addr[1])
+            logger.write("Cliente "+str(clientId)+":\n")
+            logger.write("Se ha conectado el cliente " +
+                         str(addr[0])+" al puerto "+str(addr[1])+"\n")
+            server.sendto(bytes(str(clientId), "utf-8"), addr)
+        elif data[0:20] == bytes("Ya recibi mi numero", "utf-8"):
+            id = data[20:len(data)].decode('utf-8')
+            lista.append([addr, id])
+            print("el cliente recibio su numero: " + id)
+    return lista
+
+
+def Main():
+    logger = startLogger()
+    server = startServer(logger)
+    selectedOption = printMenu(logger)
+    fielName = getFileName(selectedOption)
+    logger.write(fielName+"\n")
     hash = getHashFromFile(fielName)
     print('A cuantos usuarios desea transmitir el archivo')
-    opcionU = int(input())
-    print('Usted seleciono '+str(opcionU)+' usuarios')
-    # put the socket into listening mode 
-    print("Socket is listening") 
-    clientId = 0
-    lista = []
-    # a forever loop until client wants to exit 
-    for i in range(opcionU):
-        data, address = server.recvfrom(4096)
-        lista.append(address)
-        clientId+=1
-        print('Connected to :', address[0], ':', address[1]) 
-        log.write("Cliente "+str(clientId)+":\n")
-        log.write("Se ha conectado el cliente "+str(address[0])+" al puerto "+str(address[1])+"\n")
-        server.sendto(bytes(str(clientId),"utf-8"),address)
+    clientsNumber = int(input())
+    print('Usted seleciono '+str(clientsNumber)+' usuarios')
+    # server.listen(25)
+    print("socket is listening")
+    clients = getClients(logger, server, clientsNumber)
 
     thread_list = []
-
-    for j in range(len(lista)):
-        thread = threading.Thread(target = threaded, args = (lista[j], fielName, hash, j+1, log))
+    for j in range(len(clients)):
+        thread = threading.Thread(target=sendInfoThread, args=(
+            clients[j][0], fielName, hash, clients[j][1], logger))
         thread.start()
         thread_list.append(thread)
 
-    for thread in thread_list:
-        thread.join()
+    finishedClients = 0
+    while finishedClients < len(clients):
+        data, addr = server.recvfrom(1024)
+        if data[0:10] == bytes("Ya recibi", "utf-8"):
+            finishedClients += 1
+            clientId = data[10:len(data)].decode('utf-8')
+            print("Confimacion exitosa para cliente" + clientId)
+            logger.write(
+                "Tranferencia Exitosa hacia el cliente " + clientId+"\n")
+        elif data[0:10] == bytes("No recibi", "utf-8"):
+            finishedClients += 1
+            clientId = data[10:len(data)].decode('utf-8')
+            print("Confimacion NO exitosa para cliente" + clientId)
+            logger.write(
+                "Tranferencia NO Exitosa hacia el cliente " + clientId+"\n")
+
     print("Se termino de enviar la informacion a todos los clientes")
 
 
-  
-  
-if __name__ == '__main__': 
-    Main() 
+if __name__ == '__main__':
+    Main()
